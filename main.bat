a98a78e25d09a9ba7e091651d0b667be9d63ad4b
Paul Meyer <okovessimon@yahoo.de>
2025-04-10 16:21:11 +0200

    add batch file to simplify start

---
+++ main.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Starting direction search ...
+echo.
+.venv\Scripts\python.exe main.py
+echo.
+echo Press any key to close ...
+pause >nul
---
+++ set_config.bat
@@ -0,0 +1,1 @@
+.venv\Scripts\python.exe set_config.py
